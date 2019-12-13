820620dd3f6cdebd4d66f80bab87c57cf8d09960
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-12-13 17:11:54 +0100

    First working implementation

--- server.js
+++ server.js
@@ -1,13 +1,83 @@
+const {pipeline} = require('stream')
+const {uniqueId} = require('lodash')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const multer = require('multer')
+const contentDisposition = require('content-disposition')
+const {geocodeCsvFile} = require('./lib/geocode-csv')
 
 const app = express()
+const upload = multer()
 
 app.use(cors())
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
+
+function ensureArray(value) {
+  if (value) {
+    return Array.isArray(value) ? value : [value]
+  }
+
+  return []
+}
+
+function logFinished(obj) {
+  const finishedAt = new Date()
+  return {
+    ...obj,
+    finishedAt,
+    duration: finishedAt - obj.startedAt
+  }
+}
+
+app.post('/search/csv', upload.single('data'), (req, res) => {
+  const {originalName} = req.file
+  const geocodeOptions = req.body ? {
+    columns: ensureArray(req.body.columns),
+    citycode: req.body.citycode,
+    postcode: req.body.postcode
+  } : {}
+
+  const logObject = {
+    reqId: uniqueId('req_'),
+    status: 'created',
+    ...geocodeOptions,
+    fileSize: req.file.size,
+    startedAt: new Date()
+  }
+
+  console.log(JSON.stringify(logObject))
+
+  geocodeCsvFile(req.file.buffer, geocodeOptions, (err, geocodedStream) => {
+    if (err) {
+      return res.status(500).send({code: 500, message: err.message})
+    }
+
+    res
+      .type('csv')
+      .set('Content-Disposition', contentDisposition('geocoded-' + originalName))
+
+    pipeline(geocodedStream, res, err => {
+      if (err) {
+        res.destroy()
+        console.log(JSON.stringify({
+          ...logFinished(logObject),
+          status: 'failed',
+          error: err.message
+        }))
+        console.log(err)
+        return
+      }
+
+      console.log(JSON.stringify({
+        ...logFinished(logObject),
+        status: 'completed'
+      }))
+    })
+  })
+})
 
 app.listen(process.env.PORT || 5000)
