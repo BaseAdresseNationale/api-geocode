ec4880944464957b790909fce23a60bffccbe7b6
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-10-03 23:26:03 +0200

    Unit testing with ava

---
+++ test/addok.js
@@ -0,0 +1,24 @@
+import test from 'ava'
+import {prepareQuery} from '../lib/addok.js'
+
+test('prepareQuery / full', t => {
+  t.deepEqual(prepareQuery({
+    q: 'foobar',
+    limit: '3',
+    type: 'housenumber',
+    lon: '-1.11',
+    lat: '3.456',
+    postcode: '2B123',
+    citycode: '12345',
+    autocomplete: '1'
+  }), {
+    q: 'foobar',
+    limit: '3',
+    type: 'housenumber',
+    lon: '-1.11',
+    lat: '3.456',
+    postcode: '2B123',
+    citycode: '12345',
+    autocomplete: '1'
+  })
+})
